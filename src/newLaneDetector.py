8f329b5cd8a730e0a9742eb920ca73a4b00bdd7b
123p10 <obrake3@gmail.com>
2019-01-06 20:23:57 -0500

    OpenCV

--- src/newLaneDetector.py
+++ src/newLaneDetector.py
@@ -8,8 +8,20 @@ class NewLaneDetector:
         cannyedImage = self.cannyImage(transformedImage)
         return cannyedImage
     def transformLane(self,frame):
-        perspectiveTransform = cv2.getPerspectiveTransform(np.float32([[0,560],[0,720],[1280,720],[1280,560]]),np.float32([[0,0],[0,720],[1280,720],[1280,0]]))
+        perspectiveTransform = cv2.getPerspectiveTransform(np.float32([[0,650],[0,720],[1280,720],[1280,650]]),np.float32([[0,0],[0,720],[1280,720],[1280,0]]))
         out = cv2.warpPerspective(frame,perspectiveTransform,(1280,720))
         return out
     def cannyImage(self,frame):
         return cv2.Canny(frame,100,150)
+    def calcLocation():
+
+        return 0
+    def calcHist(self,frame):
+        hist = cv2.calcHist([frame],[0],None,[256],[0,256])
+        a = 0
+        for i in hist:
+            if i > 10:
+                cv2.line(frame,(a,0),(a,700),(255,0,0),5)
+                print(i)
+            a += 1
+        return frame
